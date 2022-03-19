cc583281a768076377211faf02028812e3b7775d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-19 09:31:42 +0800

    Delete helpers.py

--- helpers.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-def cleanhtml(raw_html):
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', raw_html)
-    cleantext = cleantext.replace("&nbsp;", " ")
-    return cleantext
